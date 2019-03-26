d5e3b4dc751cc76bee02212b6fd4884cd2c4adce
baxterjr@outlook.com <baxterjr@outlook.com>
2019-03-26 17:50:18 -0400

    more work on patch creation; should possibly make classes instead of structs for < operation is defined

--- src/Map.cpp
+++ src/Map.cpp
@@ -13,7 +13,7 @@ namespace purdue_arc
   std::pair<std::array<double[3]>, std::array<size_t[4]>> Map::GeneratePatch()
   {
     // Queue of discovered edges to visit
-    std::queue<struct HalfEdge *> edgeQueue;
+    std::queue<HalfEdge *> edgeQueue;
 
     // Set of quads keys that we have visited or have queued
     std::unordered_set<size_t> visitedQuads;
@@ -22,7 +22,7 @@ namespace purdue_arc
     std::vector<size_t[4]> quadCornerVector;
     
     // map of vertices we have visited and pointer to vertex struct
-    std::unordered_map<size_t, struct Vertex *> vertexMap;
+    std::unordered_map<size_t, Vertex *> vertexMap;
 
     // Start off with originEdge and its parent
     edgeQueue.push(&originEdge);
@@ -30,23 +30,23 @@ namespace purdue_arc
 
     while(edgeQueue.size() > 0)
     {
-      struct HalfEdge currentEdge = edgeQueue.pop();
+      HalfEdge currentEdge = edgeQueue.pop();
       size_t[4] currentCorners;
 
       // Travel around the current quad
       for (size_t jumps = 0; jumps < 4; jumps++)
       {
         // The base vertex of the current half-edge
-        struct Vertex currentVertex = currentEdge.baseVertex;
+        Vertex currentVertex = currentEdge.baseVertex;
 
         // Add vertex to list of vertices if needed
-        vertices.insert(std::pair<size_t, struct Vertex *>(currentVertex.key, &currentVertex))
+        vertexMap.insert(std::pair<size_t, Vertex *>(currentVertex.key, &currentVertex))
 
         // Add vertex's key to list of quad's vertices' keys
         currentCorners[jumps] = currentVertex.key;
 
         // Check if we have visited the twin
-        struct HalfEdge currentTwin = currentEdge.twinEdge;
+        HalfEdge currentTwin = currentEdge.twinEdge;
         if(visitedQuads.find(currentTwin.parentQuad.key) ~= visitedQuads.end())
         {
           // This quad has not been visited
@@ -63,6 +63,28 @@ namespace purdue_arc
     }
 
     // queue is empty, meaning we have visited all half edges, quads, and vertices
+    // Create a vector of the vertice's positions and sort by key
+    std::vector<Vertex *> vertexVector (vertexMap.begin(), vertexMap.end());
+    std::sort (vertexVector.begin(), vertexVector.end(),
+      [](Vertex * a, Vertex * b)
+      {
+        return a.key < b.key;
+      });
+
+    // Create a vector of indices into the above vector for each of the quads
+    std::vector<size_t[4]> patchFaces;
+    patchFaces.reserve(vertexVector.size());
+    for(auto it = quadCornerVector.begin(); it != quadCornerVector.end(); ++it)
+    {
+      size_t[4] currentCorners;
+      for (size_t jumps = 0; jumps < 4; jumps++)
+      {
+        currentCorners[jump] = std::lower_bound(vertexVector.begin(), vertexVector.end(), *it[jumps]) - quadCornerVector.begin();
+      }
+      patchFaces.push_back(currentCorners);
+    }
+
+
     //quadCornerVector
     //vertexMap
   }
