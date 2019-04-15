f144df3c057f4f73cea2304ac34f5f20fc0bcc4b
James Baxter <baxter26@purdue.edu>
2019-04-14 22:45:12 -0400

    map changes

--- include/exploration_manifold/Map.h
+++ include/exploration_manifold/Map.h
@@ -1,49 +1,57 @@
-namespace purdue_arc
-{
-    struct HalfEdge
-    {
-      struct Vertex * baseVertex;
-      struct Quad * parentQuad;
-
-      struct HalfEdge * prevEdge;
-      struct HalfEdge * twinEdge;
-      struct HalfEdge * nextEdge;
-
-      size_t key;
-    };
-
-    struct Vertex
-    {
-      double position_x;
-      double position_y;
-      double position_z;
-
-      size_t key;
-    };
-
-    struct Quad
-    {
-      double normal_x;
-      double normal_y;
-      double normal_z;
-
-      double variance;
-      double certainty;
-
-      size_t key;
-    }
-
 class Map {
+  struct mapGeometry
+  {
+    size_t key;
+
+    bool operator<(meshGeometry other) const
+    {
+      return other.key < this.key;
+    }
+  };
+
+  struct mapHalfEdge : mapGeometry
+  {
+    mapVertex * baseVertex;
+    mapQuad * parentQuad;
+
+    mapHalfEdge * prevEdge;
+    mapHalfEdge * twinEdge;
+    mapHalfEdge * nextEdge;
+  };
+
+  struct mapVertex : mapGeometry
+  {
+    double position_x;
+    double position_y;
+    double position_z;
+  };
+
+  struct mapQuad : mapGeometry
+  {
+    double normal_x;
+    double normal_y;
+    double normal_z;
+
+    double variance;
+    double certainty;
+  };
 public:
     Map();
     ~Map();
 
-    std::pair<std::array<double[3]>, std::array<size_t[4]>> Map::GeneratePatch();;
+    std::pair<std::array<double[3]>, std::array<size_t[4]>> Map::GeneratePatch();
+
+    // Add data, must be linked to existing stuff I guess.
+    bool extendMap();
 
 private:
-  struct HalfEdge originEdge;
+  // Current half edges
+  // fronteirs?
+  // zeroEdge?
+
+
+
+  mapHalfEdge * originEdgePt;
   unsigned int numQuads;
 
-
 };
-}
--- src/Map.cpp
+++ src/Map.cpp
@@ -1,91 +1,89 @@
 include "exploration_manifold/Quad.h"
 
-namespace purdue_arc
+Map::Map()
 {
+  numQuads = 0;
+  originEdgePt = NULL
+}
 
-  Map::Map()
+Map::~Map() = default;
+
+std::pair<std::array<double[3]>, std::array<size_t[4]>> Map::GeneratePatch()
+{
+  // Queue of discovered edges to visit
+  std::queue<mapHalfEdge *> edgeQueue;
+
+  // Set of quads keys that we have visited or have queued
+  std::unordered_set<size_t> visitedQuads;
+
+  // Vector of quad's vertices' keys that we have visited
+  std::vector<size_t[4]> quadCornerKeys;
+
+  // map of vertices we have visited and pointer to vertex struct
+  std::unordered_map<size_t, mapVertex *> vertexMap;
+
+  // Start off with originEdge and its parent
+  edgeQueue.push(originEdgePt);
+  visitedQuads.insert(*originEdgePt.parentQuad.key);
+
+  while(not edgeQueue.empty())
   {
-    numQuads = 0;
-  }
+    HalfEdge * currentEdgePt = *edgeQueue.front();
+    edgeQueue.pop();
+    size_t[4] currentCorners;
 
-  Map::~Map() = delete();
-
-  std::pair<std::array<double[3]>, std::array<size_t[4]>> Map::GeneratePatch()
-  {
-    // Queue of discovered edges to visit
-    std::queue<HalfEdge *> edgeQueue;
-
-    // Set of quads keys that we have visited or have queued
-    std::unordered_set<size_t> visitedQuads;
-
-    // Vector of quad's vertices' keys that we have visited
-    std::vector<size_t[4]> quadCornerVector;
-    
-    // map of vertices we have visited and pointer to vertex struct
-    std::unordered_map<size_t, Vertex *> vertexMap;
-
-    // Start off with originEdge and its parent
-    edgeQueue.push(&originEdge);
-    visitedQuads.insert(originEdge.parentQuad.key);
-
-    while(edgeQueue.size() > 0)
+    // Travel around the current quad
+    for (size_t jumps = 0; jumps < 4; jumps++)
     {
-      HalfEdge currentEdge = edgeQueue.pop();
-      size_t[4] currentCorners;
+      // The base vertex of the current half-edge
+      Vertex currentVertex = currentEdge.baseVertex;
 
-      // Travel around the current quad
-      for (size_t jumps = 0; jumps < 4; jumps++)
+      // Add vertex to list of vertices if needed
+      vertexMap.insert(std::pair<size_t, Vertex *>(currentVertex.key, &currentVertex))
+
+      // Add vertex's key to list of quad's vertices' keys
+      currentCorners[jumps] = currentVertex.key;
+
+      // Check if we have visited the twin
+      HalfEdge currentTwin = currentEdge.twinEdge;
+      if(visitedQuads.find(currentTwin.parentQuad.key) ~= visitedQuads.end())
       {
-        // The base vertex of the current half-edge
-        Vertex currentVertex = currentEdge.baseVertex;
-
-        // Add vertex to list of vertices if needed
-        vertexMap.insert(std::pair<size_t, Vertex *>(currentVertex.key, &currentVertex))
-
-        // Add vertex's key to list of quad's vertices' keys
-        currentCorners[jumps] = currentVertex.key;
-
-        // Check if we have visited the twin
-        HalfEdge currentTwin = currentEdge.twinEdge;
-        if(visitedQuads.find(currentTwin.parentQuad.key) ~= visitedQuads.end())
-        {
-          // This quad has not been visited
-          visitedQuads.insert(currentTwin.parentQuad.key);
-          edgeQueue.push(&currentTwin);
-        }
-
-        // Move to next half-edge
-        currentEdge = currentEdge.nextEdge;
+        // This quad has not been visited
+        visitedQuads.insert(currentTwin.parentQuad.key);
+        edgeQueue.push(&currentTwin);
       }
 
-      // Add vertex data for quad to vector
-      quadCornerVector.insert(currentCorners);
+      // Move to next half-edge
+      currentEdge = currentEdge.nextEdge;
     }
 
-    // queue is empty, meaning we have visited all half edges, quads, and vertices
-    // Create a vector of the vertice's positions and sort by key
-    std::vector<Vertex *> vertexVector (vertexMap.begin(), vertexMap.end());
-    std::sort (vertexVector.begin(), vertexVector.end(),
-      [](Vertex * a, Vertex * b)
-      {
-        return a.key < b.key;
-      });
-
-    // Create a vector of indices into the above vector for each of the quads
-    std::vector<size_t[4]> patchFaces;
-    patchFaces.reserve(vertexVector.size());
-    for(auto it = quadCornerVector.begin(); it != quadCornerVector.end(); ++it)
-    {
-      size_t[4] currentCorners;
-      for (size_t jumps = 0; jumps < 4; jumps++)
-      {
-        currentCorners[jump] = std::lower_bound(vertexVector.begin(), vertexVector.end(), *it[jumps]) - quadCornerVector.begin();
-      }
-      patchFaces.push_back(currentCorners);
-    }
-
-
-    //quadCornerVector
-    //vertexMap
+    // Add vertex data for quad to vector
+    quadCornerVector.insert(currentCorners);
   }
+
+  // queue is empty, meaning we have visited all half edges, quads, and vertices
+  // Create a vector of the vertice's positions and sort by key
+  std::vector<Vertex *> vertexVector (vertexMap.begin(), vertexMap.end());
+  std::sort (vertexVector.begin(), vertexVector.end(),
+    [](Vertex * a, Vertex * b)
+    {
+      return a.key < b.key;
+    });
+
+  // Create a vector of indices into the above vector for each of the quads
+  std::vector<size_t[4]> patchFaces;
+  patchFaces.reserve(vertexVector.size());
+  for(auto it = quadCornerVector.begin(); it != quadCornerVector.end(); ++it)
+  {
+    size_t[4] currentCorners;
+    for (size_t jumps = 0; jumps < 4; jumps++)
+    {
+      currentCorners[jump] = std::lower_bound(vertexVector.begin(), vertexVector.end(), *it[jumps]) - quadCornerVector.begin();
+    }
+    patchFaces.push_back(currentCorners);
+  }
+
+
+  //quadCornerVector
+  //vertexMap
 }
