b8e7c57b398bf7dc4713ed98eb14321c37221de8
James Baxter <baxter26@purdue.edu>
2019-04-23 23:10:45 -0400

    compilation and test stuff

---
+++ src/test.cpp
@@ -0,0 +1,11 @@
+#include "exploration_manifold/HalfEdge.h"
+#include "exploration_manifold/Map.h"
+
+int main(){
+  double test_x [3] = {1, 0, 0};
+  double test_y [3] = {0, 1, 0};
+
+  Map map = Map(test_x, test_y);
+
+  return 0;
+}
