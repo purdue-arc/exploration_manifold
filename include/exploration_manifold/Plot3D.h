ed08ef81fc8b1ee88903e96dad41de1980423105
James Baxter <baxter26@purdue.edu>
2019-04-25 12:49:33 -0400

    initial thoughts on gnuplot interface

---
+++ include/exploration_manifold/Plot3D.h
@@ -0,0 +1,40 @@
+#include "gnuplot-iostream.h"
+#include <array>
+
+class Plot3D{
+public:
+  struct Polygon
+  {
+    std::array<double, 3> vertex_1;
+    std::array<double, 3> vertex_2;
+    std::array<double, 3> vertex_3;
+    std::array<double, 3> vertex_4;
+    Polygon(std::array<double, 3> v1, std::array<double, 3> v2, std::array<double, 3> v3, std::array<double, 3> v4)
+    {
+      vertex_1 = v1;
+      vertex_2 = v2;
+      vertex_3 = v3;
+      vertex_4 = v4;
+    }
+  };
+
+  Plot3D();
+  ~Plot3D();
+
+  void addPolygon(Polygon poly);
+  void clearPlot();
+
+private:
+  void updatePlot();
+  bool updateRanges(std::array<double, 3> vertex);
+  void sendRanges();
+
+  Gnuplot plot;
+  unsigned int numObjects;
+  double max_x;
+  double max_y;
+  double max_z;
+  double min_x;
+  double min_x;
+  double min_x;
+};
---
+++ src/Plot3D.cpp
@@ -0,0 +1,82 @@
+#include "exploration_manifold/Plot3D.h"
+
+Plot3D::Plot3D()
+{
+  max_x = 10;
+  max_y = 10;
+  max_z = 10;
+  min_x = -10;
+  min_x = -10;
+  min_x = -10;
+
+  sendRanges();
+  updatePlot();
+}
+
+~Plot3D::Plot3D() = default;
+
+void Plot3D::addPolygon(Polygon poly){
+  numObjects++;
+  plot << "set object " << numObjects << " polygon from '-' to '-' to '-' to '-' to '-' fillsytle transparent solid 0.5\n";
+  gp.send1d(poly.v1);
+  gp.send1d(poly.v2);
+  gp.send1d(poly.v3);
+  gp.send1d(poly.v4);
+  gp.send1d(poly.v1);
+
+  if( updateRanges(poly.v1) || updateRanges(poly.v2) || updateRanges(poly.v3) || updateRanges(poly.v4)){
+    sendRanges();
+  }
+  
+  updatePlot();
+}
+
+void Plot3D::updatePlot(){
+  plot << "splot " << numObjects << "/0 notitle\n";
+}
+
+void Plot3D::clearPlot(){
+  plot << "clear\n";
+  numObjects = 0;
+}
+
+bool Plot3D::updateRanges(std::array<double, 3> vertex){
+  bool change = false;
+  if (vertex[0] > x_max)
+  {
+    x_max = vertex[0];
+    change = true;
+  }
+  else if (vertex[0] < x_min)
+  {
+    x_min = vertex[0];
+    change = true;
+  }
+  if (vertex[1] > y_max)
+  {
+    y_max = vertex[1];
+    change = true;
+  }
+  else if (vertex[1] < y_min)
+  {
+    y_min = vertex[1];
+    change = true;
+  }
+  if (vertex[2] > z_max)
+  {
+    z_max = vertex[2];
+    change = true;
+  }
+  else if (vertex[2] < z_min)
+  {
+    z_min = vertex[2];
+    change = true;
+  }
+  return change;
+}
+
+void Plot3D::sendRanges(){
+  plot << "set xrange [" << min_x << ":" << max_x << "]\n";
+  plot << "set yrange [" << min_y << ":" << max_y << "]\n";
+  plot << "set zrange [" << min_z << ":" << max_z << "]\n";
+}
