499f0b6c14291087ecf884c44e835f9172595b85
James Baxter <baxter26@purdue.edu>
2019-04-26 11:45:45 -0400

    works with simple test case

--- src/test.cpp
+++ src/test.cpp
@@ -3,16 +3,25 @@
 #include <iostream>
 
 int main(){
-  double test_x [3] = {1, 0, 0};
-  double test_y [3] = {0, 1, 0};
+  double test_x [3] = {5, 0, 1};
+  double test_y [3] = {0, 7, -3};
 
   Map map = Map(test_x, test_y);
 
   Plot3D plot;
 
-  Plot3D::Polygon poly ({-5,-5,-1}, {-5,5,0}, {5,5,1}, {5,-5,0});
-  plot.addPolygon(poly);
+  // Plot3D::Polygon poly ({-5,-5,-1}, {-5,5,0}, {5,5,1}, {5,-5,0});
+  // plot.addPolygon(poly);
+
+  std::vector<std::array<std::array<double, 3>, 4>> geometry = map.exportGeometry();
+
+  for(auto itr = geometry.begin(); itr < geometry.end(); ++itr){
+    Plot3D::Polygon poly ((*itr)[0], (*itr)[1], (*itr)[2], (*itr)[3]);
+    plot.addPolygon(poly);
+  }
 
   // keep it running so the gnuplot is responsive
+  std::cout << "waiting" << std::endl;
+  std::cin.get();
   return 0;
 }
